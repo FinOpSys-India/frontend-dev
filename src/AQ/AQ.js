fc992ce9b085a2728eab6f747224db3cf5d44271
parassingh898 <psingh@finopsys.ai>
2024-09-28 14:10:32 +0530

    added Upload modal frontend functionality

--- src/AQ/AQ.js
+++ src/AQ/AQ.js
@@ -5,8 +5,9 @@ import { apiEndPointUrl } from "../utils/apiService";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; 
 import { useDropzone } from 'react-dropzone';
-import uploadLogo from '../assets/uploadLogo.svg'
-const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
+import uploadLogo from '../assets/uploadLogo.jpeg';
+import billsLogo from '../assets/bills.svg'
+const MAX_FILE_SIZE = 500 * 1024 * 1024; // 10MB
 
 function AQ() {
   const [showModal, setShowModal] = useState(false);
@@ -51,7 +52,7 @@ function AQ() {
     formData.append("file", fileToUpload);
 
     try {
-      const response = await axios.post('http://localhost:9000/upload', formData, {
+      const response = await axios.post(`${apiEndPointUrl}/upload`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -66,14 +67,12 @@ function AQ() {
 
       if (response.status === 200) {
         setUploadStatus("success");
-        setTimeout(() => {
-          handleModalClose(); // Close modal after successful upload
-        }, 2000); // Close the modal after a short delay
-        toast.success("File uploaded successfully!", { autoClose: 1500 });
+
+         toast.success("File uploaded successfully!", { autoClose: 1500 });
       }
     } catch (error) {
       if (error.response && error.response.status === 400) {
-        toast.error("File size exceeds 10MB", { autoClose: 1500 });
+        toast.error("File size exceeds 500MB", { autoClose: 1500 });
         setUploadStatus("error");
       } else {
         toast.error("File upload failed", { autoClose: 1500 });
@@ -110,13 +109,14 @@ function AQ() {
               textAlign: 'center',
               cursor: 'pointer',
               backgroundColor: isDragActive ? '#f3f3f3' : 'white',
-              width:"92%",
-              marginLeft:"4%",
-               borderRadius:'3%'
+              width:"93%",
+              marginLeft:"3%",
+               borderRadius:'12.23px'
             }}
-          ><div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
-          <img src={uploadLogo} />
-        </div>
+          >
+            {/* <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}> */}
+          <img src={uploadLogo} style={{ height:"100px", width:"100px" }}/>
+        {/* </div> */}
             <input {...getInputProps()} />
             {isDragActive ? (
               <p>Drop the file here...</p>
@@ -124,18 +124,23 @@ function AQ() {
               <p>Drag and drop your file here or <span style={{ color: 'blue', textDecoration: 'underline' }}>Browse file</span></p>
             )}
           </div>
-          <div style={{marginTop:"2%",display:"flex", flexDirection:"row", justifyContent:"space-between",width: '90%', marginLeft:"5%"}}>            
+          <div style={{marginTop:"2%",display:"flex", flexDirection:"row", justifyContent:"space-between",width: '94%', marginLeft:"3%"}}>            
             <p>Supported formats: PDF, DOC, XLSX & JPEG.</p>
-            <p>Max file size: 10MB</p>
+            <p>Max file size: 500MB</p>
           </div>
           {file && (
-            <div style={{ marginTop: '20px', border: '2px solid #7939EF', borderRadius:'3%' }}>
-              <p>{file.name} ({(file.size / (1024 * 1024)).toFixed(2)}MB)</p>
+            <div style={{display:"flex", marginTop: '20px', border: `2px solid ${uploadStatus === 'success' ? '#208348' : '#7939EF'}`, borderRadius:"12.23px",padding:"2%" ,width: '94%', marginLeft:"3%"}}>
+              <img src={billsLogo} style={{height:"24.46px", width:"24.46px", margin:"3% 1.5% 1.5% 1%"}}/>
+              <div>
+              <p style={{ font:"Inter,, sans-serif", color:"#141414", fontWeight:"500", fontSize:'14.67px'}} >{file.name} ({(file.size / (1024 * 1024)).toFixed(2)}MB)</p>
               <ProgressBar
                 now={progress}
                 label={`${progress}%`}
-                variant={uploadStatus === 'error' ? 'danger' : progress === 100 ? 'success' : 'info'}
+                variant={uploadStatus === 'error' ? 'danger' : progress === 100 ? 'success' : ''}
+                style={{ height: '11px', width:"85%", font:"Inter,, sans-serif", color:"#141414", fontWeight:"500"}} 
+                 
               />
+              </div>
             </div>
           )}
         </Modal.Body>
